aa760342367926155b31e1aaa9c769c1a6f60000
Balaji Mergu <mergubalaji@gmail.com>
2024-04-22 12:14:57 -0400

    Assignment 2 - GlobalExceptionHandler

--- springboot-microservices/department-service/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentServiceImpl.java
+++ springboot-microservices/department-service/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.departmentservice.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.departmentservice.dto.DepartmentDto;
 import net.javaguides.departmentservice.entity.Department;
+import net.javaguides.departmentservice.exception.ResourceNotFoundException;
 import net.javaguides.departmentservice.mapper.DepartmentMapper;
 import net.javaguides.departmentservice.repository.DepartmentRepository;
 import net.javaguides.departmentservice.service.DepartmentService;
@@ -30,7 +31,12 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Override
     public DepartmentDto getDepartmentByCode(String departmentCode) {
 
-        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+        Department department = null;
+        try {
+            department = departmentRepository.findByDepartmentCode(departmentCode);
+        } catch (Exception exception) {
+            throw new ResourceNotFoundException("Departement","departmentCode", Long.parseLong(departmentCode));
+        }
 
         DepartmentDto departmentDto = DepartmentMapper.mapToDepartmentDto(department);
 
